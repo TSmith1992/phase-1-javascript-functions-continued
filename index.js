8d46dc5493f7b4b298af06222e80b4d769b3209c
Tarique Smith <tpsmith1992@gmail.com>
2021-07-19 13:07:34 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(act='roller-skate'){
+	return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act1='go to the office'){
+	return `This Monday, I will ${act1}.`
+}
+
+function wrapAdjective(char='*'){
+	return function inner(defaul='special'){
+	return `You are ${char}${defaul}${char}!`
+	}
+}
+
+wrapAdjective("||")("a dedicated programmer")
+wrapAdjective("*")("a dedicated programmer")
